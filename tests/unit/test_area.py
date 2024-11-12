a93ce275e04c56439ce5006cc15b529349d25b6b
unknown <meryamerrami2@gmail.com>
2024-11-12 12:08:55 +0100

    update

--- tests/unit/test_area.py
+++ tests/unit/test_area.py
@@ -1,8 +1,8 @@
 from langchain.schema import HumanMessage
 import pytest
-from area import graph
-import sys
-sys.path.append('./src')
+from tools_agents_build import graph
+# import sys
+# sys.path.append('./src')
 # Cas de test prédéfinis
 test_cases = [
     {"input": "definition of machine learning", "expected": "Researcher"},
